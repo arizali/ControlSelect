96ba2a0de4bcf6fa6d56e3c3320dc9b6f8de8864
arizali <alp3r3r@gmail.com>
2014-06-05 22:01:29 +0300

    Updated select.js
    Updated Select.js for better performance

--- select.js
+++ select.js
@@ -2,30 +2,29 @@
     $.fn.SelectControl = function( options ) {
          var settings = $.extend({
             current: "data-current",
-            dataUrl: "get.php",
+            dataUrl: "options.php",
             wrapper: "#option",
-            append: '<option value="">Tüm Türler</option>',
-			idtitle: "id" 
+            append: '<option value="">Some Options</option>',
+			idtitle: "id",
+			id2title: "id2" 
         }, options );
  
 		var id = this.attr(settings.current);
+		var id2 = $(settings.wrapper).attr(settings.current);
 		if(id==0 || id=="") {
 		$(settings.wrapper).attr("disabled","disabled");	
 		$(settings.wrapper).html(settings.append);
 		} else {
+		if(id2==""||id2==0){ var dataString = settings.idtitle+"=" + id; } else { var dataString = settings.idtitle+"=" + id+"&"+settings.id2title+"="+id2; }
 		$.ajax
 		({	
 		type: "POST",
 		url: settings.dataUrl,
-		data: settings.idtitle+"=" + id,
+		data: dataString,
 		cache: false,
 		success: function(html)
-		{
-		$(settings.wrapper).removeAttr("disabled");	
-		$(settings.wrapper).html(settings.append+html);
-		} 
-		});
-		}			 
-    };
+		{ $(settings.wrapper).removeAttr("disabled");	
+		  $(settings.wrapper).html(html);
+		} }); }	};
  
 }( jQuery ));
